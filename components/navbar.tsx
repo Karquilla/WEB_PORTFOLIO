0073899a78bd31d5f56c14fdbce95470fc1090a9
kyle arquilla <k2snowboarder21@gmail.com>
2024-11-21 18:46:48 -0500

    fixes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,11 +4,11 @@ import  Link  from "next/link";
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+  
   return (
-    
       <nav>
       <ul>
-        <div className={styles.control_container}>
+        <div className={styles.sidebar}>
         <li ><Link href="/Home" className={styles.control_button}><span>Home</span></Link></li>
         <li ><Link href="/AStar" className={styles.control_button}>A-Star</Link></li>
         </div>
